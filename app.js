d8a220b4dbb485a6922c5e41830738249878724f
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-13 19:35:25 +0530

    Added cors

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const express = require("express");
 const todoRoutes = require('./routes/todoRoutes')
 const taskRoutes = require('./routes/taskRoutes')
 const searchRoutes = require('./routes/searchRoutes')
+const cors = require("cors");
 const app = express();
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(cors());
 
 app.use('/todo', todoRoutes);
 app.use('/todo', taskRoutes);
